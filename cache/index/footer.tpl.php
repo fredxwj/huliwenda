ab55fb4f2aaeb1f07c6b533ee4fba94e4c62144a
fredxwj <xwj1985@gmail.com>
2019-01-09 15:25:54 +0800

    remove unused file

--- cache/index/footer.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php defined('IN_YZMPHP') or exit('No permission resources.'); ?><div class="clearfix"></div>		 
-<footer>
-  <p><?php echo $site['site_copyright'];?> <a href="http://www.yzmcms.com/" target="_blank">帅气的采用了YzmCMS</a> <?php echo $site['site_filing'];?> <a href="<?php echo U('guestbook/index/init');?>" target="_blank">留言板</a></p>
-</footer>
-<a href="javascript:void" class="cd-top">Top</a>
-<div class="none"><?php echo $site['site_code'];?></div>
-</body>
-</html>
